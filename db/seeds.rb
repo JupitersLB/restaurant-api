897e7174152f7b14ce35cdc9a5a82e625cdc4529
Liam Baker <liam.baker2310@gmail.com>
2020-10-07 15:40:54 +0900

    seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database"
+Menu.delete_all
+User.delete_all
+MenuItem.delete_all
+puts "Cleaned"
+
+puts "Creating users"
+params = {}
+params[:email] = 'liam@restaurant.com'
+params[:password] = 'password'
+params[:role] = 'admin'
+new_user = User.new(params)
+new_user.save
+puts "Created user #{new_user.id}"
+
+params = {}
+params[:email] = 'markie@restaurant.com'
+params[:password] = 'password'
+params[:role] = 'staff'
+new_user = User.new(params)
+new_user.save
+puts "Created user #{new_user.id}"
+
+params = {}
+params[:email] = 'david@restaurant.com'
+params[:password] = 'password'
+params[:role] = 'customer'
+new_user = User.new(params)
+new_user.save
+puts "Created user #{new_user.id}"
+
+puts "Users created!"
+
