fa521c8cf792a0c3be32919e3acf678dbad1fe44
Liam Baker <liam.baker2310@gmail.com>
2020-10-08 14:29:58 +0900

    order total price increments when new item ordered

--- app/controllers/api/v1/order_items_controller.rb
+++ app/controllers/api/v1/order_items_controller.rb
@@ -2,9 +2,13 @@ class Api::V1::OrderItemsController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, only: :create
 
   def create
+    order = Order.find(params[:order_id])
     @order_item = OrderItem.new(order_params)
+    @order_item.order = order
     authorize @order_item
     if @order_item.save
+      order.total_price += @order_item.menu_item.price
+      order.save
       redirect_to api_v1_order_path(@order_item.order)
     else
       render_error
