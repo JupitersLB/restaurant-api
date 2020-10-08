934fd864572f57bf80cb99dfef8395244c32fd40
Liam Baker <liam.baker2310@gmail.com>
2020-10-08 13:01:38 +0900

    added updated action for order

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::OrdersController < Api::V1::BaseController
+  before_action :set_order, only: [ :show, :update ]
   acts_as_token_authentication_handler_for User
 
   def index
@@ -7,8 +8,29 @@ class Api::V1::OrdersController < Api::V1::BaseController
   end
 
   def show
-    order = Order.find(params[:id])
-    authorize order
     render json: order
+  end
+
+  def update
+    if order.update(order_params)
+      render 'menu_items/show'
+    else
+      render_error
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+    authorize order
+  end
+
+  def order_params
+    params.require(:order).permit(:menu_item)
+  end
+
+  def render_error
+    render json: { errors: @order.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
