4050f6d6c19137ee0eb18ca71f1af9c34720555d
Liam Baker <liam.baker2310@gmail.com>
2020-10-21 13:59:12 +0900

    improved menu item display

--- app/javascript/customerSide/components/menuItem.jsx
+++ app/javascript/customerSide/components/menuItem.jsx
@@ -22,9 +22,16 @@ const MenuItem = (props) => {
   const { item, email, token } = props;
   return (
     <div className="menu-item-card shadow" id={`menu-item-${item.id}`} onClick={handleClick}>
-      <p><b>{item.name}</b></p>
-      <p>{item.description}</p>
-      <p>¥{item.price}</p>
+      <img className="menu-item-image" src={item.image_url} alt=""></img>
+      <div className="menu-item-info">
+        <div className="menu-item-info-details col-9">
+          <p><b>{item.name}</b></p>
+          <p>{item.description}</p>
+        </div>
+        <div className="menu-item-info-price d-flex align-items-center justify-content-end col-3">
+          <p>¥{item.price}</p>
+        </div>
+      </div>
       <div className="btn btn-primary d-none" id={item.id} onClick={handleOrder}>
         <p>Order!</p>
       </div>
