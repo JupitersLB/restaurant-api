465b8f48191fa8d8f47fbe125aa6e3cc01594b57
Liam Baker <liam.baker2310@gmail.com>
2020-10-21 11:29:43 +0900

    improved category list design

--- app/javascript/customerSide/components/category.jsx
+++ app/javascript/customerSide/components/category.jsx
@@ -4,12 +4,19 @@ const Category = (props) => {
   const { category, id, changeCategory } = props;
 
   const handleClick = () => {
+    const cards = document.querySelectorAll('.category-card');
+    cards.forEach(card => {
+      card.classList.remove('active')
+    })
+    const card = document.querySelector(`#category-${id}`);
+    card.classList.add('active');
     changeCategory(id);
+
   }
 
   return (
-    <div className="category-card shadow" id={id} onClick={handleClick}>
-      <p>{category}</p>
+    <div className="category-card" id={`category-${id}`} onClick={handleClick}>
+      <h4>{category}</h4>
     </div>
   );
 }
--- app/javascript/customerSide/containers/categoryList.jsx
+++ app/javascript/customerSide/containers/categoryList.jsx
@@ -7,9 +7,15 @@ export default class CategoryList extends Component {
   render() {
     const { menu, changeCategory }  = this.props
     return (
-      <div className="category-list-container">
-        { menu.map( (category, idx) => <Category category={Object.keys(category)} id={idx} changeCategory={changeCategory} key={idx} />) }
-      </div>
+      <>
+        <div className="restaurant-header pt-2">
+          <h2 className="mb-0">Jupiters</h2>
+          <img className="avatar ml-3" src="../../assets/jupiter.png"></img>
+        </div>
+        <div className="category-list-container">
+          { menu.map( (category, idx) => <Category category={Object.keys(category)} id={idx} changeCategory={changeCategory} key={idx} />) }
+        </div>
+      </>
     );
   }
 }
