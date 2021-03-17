383c289e76a4506775ebbd139275fa80e5342986
Liam Baker <liam.baker2310@gmail.com>
2021-03-17 17:54:09 +0900

    added comments and removed logs for understanding

--- app/javascript/components/addItem.js
+++ app/javascript/components/addItem.js
@@ -1,23 +1,23 @@
-// import { initSelect2 } from './init_select2';
-
 const addMenuItem = () => {
+  // button used for adding an item
   const createButton = document.getElementById("addItemBtn");
-  console.log('hello')
-  createButton.addEventListener("click", event => {
-    console.log('click')
 
+  //listens for a click event
+  createButton.addEventListener("click", event => {
+
+    // selects the container for the menuItems and gets the id of the last menuItem element
     const lastId = document.querySelector(".menuItemContainer").lastElementChild.id;
 
+    // increases the id by one to work with rails naming conventions
     const newId = parseInt(lastId, 10) + 1;
 
+    // selects the first menuItem element and replaces all references of 0 with the newId
     const newMenuItem = document.querySelector('[id="0"]').outerHTML.replace(/0/g,newId);
 
+    // inserts the newMenuItem element at the bottom of the menuItemContainer
     document.querySelector(".menuItemContainer").insertAdjacentHTML(
         "beforeend", newMenuItem
     );
-    // document.querySelector(`[id="${newId}"] span`).remove()
-
-    // initSelect2();
   });
 }
 export { addMenuItem }
